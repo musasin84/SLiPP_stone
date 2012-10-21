5ad26f77ba052c0dc495065f5f28aa0651cffdba
MSi <MSi@ykkim-MSi-CR620>
2012-10-21 21:27:45 +0900

    TEST

--- SLiPP_stone/src/Copy of HelloWorld.java
+++ SLiPP_stone/src/Copy of HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("안녕하세요?xxxxx");
+		System.out.println("안녕하세요?xxZZZZZZZxxx");
 	}
 
 }
