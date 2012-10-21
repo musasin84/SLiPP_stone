f3d9c9deed929376b06f5c1666483c66ea97217a
MSi <MSi@ykkim-MSi-CR620>
2012-10-21 21:26:31 +0900

    TEST

---
+++ SLiPP_stone/src/Copy of HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+	/**
+	 * Hello World를 찍는다.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("안녕하세요?xxxxx");
+	}
+
+}
