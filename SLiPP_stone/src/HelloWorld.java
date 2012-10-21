276ceba29e353272f707d4b2f649ddc7426125b6
Kim Yeon Suk <musasin84@hanmail.net>
2012-10-21 20:51:06 +0900

    Update SLiPP_stone/src/HelloWorld.java
    test

--- SLiPP_stone/src/HelloWorld.java
+++ SLiPP_stone/src/HelloWorld.java
@@ -1,4 +1,3 @@
-
 public class HelloWorld {
 
 	/**
@@ -6,7 +5,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("안녕하세요?");
+		System.out.println("안녕하세요?xxxxx");
 	}
 
 }
