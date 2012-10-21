b0f4f87e65eefa4b2246c8daa7a0ac2af9f53d9b
MSi <MSi@ykkim-MSi-CR620>
2012-10-21 21:35:23 +0900

    TET

--- SLiPP_stone/src/Copy of HelloWorld.java
+++ SLiPP_stone/src/Copy of HelloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("ZZZZZZD");
 		System.out.println("1111111111XXFG11111");
 	}
 
